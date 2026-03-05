280801d952066c2427fc4b754aebdc79a79d11ef
doctigo39-2026 <doctigo39@gmail.com>
2026-03-05 21:12:21 +0600

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
+import "./globals.css";
+import BookingPage from "./BookingPage";
+
+export default function Home() {
+  return <BookingPage />;
+}
